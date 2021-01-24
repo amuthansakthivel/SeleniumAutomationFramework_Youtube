03e72c7d1201724848c0218f4c0a1bd7f30985a4
asakthiv <amuthan.sakthivel@dhl.com>
2021-01-24 10:27:17 +0530

    fixed the config typo

--- src/main/java/com/tmb/constants/FrameworkConstants.java
+++ src/main/java/com/tmb/constants/FrameworkConstants.java
@@ -30,7 +30,7 @@ public final class FrameworkConstants {
 	private static final String RESOURCESPATH = System.getProperty("user.dir")+"/src/test/resources";
 	private static final String CHROMEDRIVERPATH = RESOURCESPATH+"/executables/chromedriver.exe";
 	private static final String GECKODRIVERPATH = RESOURCESPATH+"/executables/geckodriver.exe";
-	private static final String CONFIGFILEPATH = RESOURCESPATH+"/config/confi.properties";
+	private static final String CONFIGFILEPATH = RESOURCESPATH+"/config/config.properties";
 	private static final String JSONCONFIGFILEPATH = RESOURCESPATH + "/config/config.json";
 	private static final String EXCELPATH = RESOURCESPATH+"/excel/testdata.xlsx";
 	private static final String RUNMANGERSHEET = "RUNMANAGER";
