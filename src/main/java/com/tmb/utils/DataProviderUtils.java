c5e821fc5b0b3e8b728f55082b4acc2b5b3ff90e
Amuthan <mech.amuthansakthivel@gmail.com>
2021-07-22 10:00:46 +0530

    Optimised Data Provider Utils code with RemoveIf

--- src/main/java/com/tmb/utils/DataProviderUtils.java
+++ src/main/java/com/tmb/utils/DataProviderUtils.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 import org.testng.annotations.DataProvider;
 
@@ -44,26 +45,23 @@ public final class DataProviderUtils {
 	 * contains the test data needed to run the iterations.
 	 *  
 	 * 
-	 * @see com.tmb.tests.AmazonDemoTest 
+	 * @see com.tmb.tests;
 	 * @see com.tmb.listeners.AnnotationTransformer
 	 */
 	@DataProvider(parallel=false)
 	public static Object[] getData(Method m) {
 		String testname = m.getName();
+
 		if(list.isEmpty()) {
 			list = ExcelUtils.getTestDetails(FrameworkConstants.getIterationDatasheet());
 			System.out.println(list);
 		}
-		
-		List<Map<String, String>> smalllist = new ArrayList<>();
-		
-		for(int i=0;i<list.size();i++) {
-			if(list.get(i).get("testname").equalsIgnoreCase(testname) &&  
-					list.get(i).get("execute").equalsIgnoreCase("yes")) {
-						smalllist.add(list.get(i));
-			}
-		}
-		System.out.println(smalllist);
+		List<Map<String, String>> smalllist = new ArrayList<>(list);
+
+		Predicate<Map<String,String>> isTestNameNotMatching = map ->!map.get("testname").equalsIgnoreCase(testname);
+		Predicate<Map<String,String>> isExecuteColumnNo = map -> map.get("execute").equalsIgnoreCase("no");
+
+		smalllist.removeIf(isTestNameNotMatching.or(isExecuteColumnNo));
 		return smalllist.toArray();
 		
 	}
